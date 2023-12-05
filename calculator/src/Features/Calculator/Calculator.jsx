39d80200f636feddcc11a756ef1a08538653275d
tu83bnh9m@code.edu.az <tu83R2GJ2@code.edu.az>
2023-12-05 08:20:52 +0400

    added

--- calculator/src/Features/Calculator/Calculator.jsx
+++ calculator/src/Features/Calculator/Calculator.jsx
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { bolme, cixma, reset, toplama, vurma } from './CalculatorSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bolme, cixma, reset, toplama, vurma } from "./CalculatorSlice";
 
 function App() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
+  const [input2, setInput2] = useState("");
   const dispatch = useDispatch();
   const result = useSelector((state) => state.calculator.result);
+  console.log(result);
 
   const handleOperation = (operator) => {
-    const number = parseFloat(input);
+    const number = { input: parseFloat(input), input2: parseFloat(input2) };
     if (number) {
       switch (operator) {
-        case '+':
+        case "+":
           dispatch(toplama(number));
           break;
-        case '-':
+        case "-":
           dispatch(cixma(number));
           break;
-        case '*':
+        case "*":
           dispatch(vurma(number));
           break;
-        case '/':
+        case "/":
           dispatch(bolme(number));
           break;
         default:
           break;
       }
     }
-    setInput('');
+    setInput("");
+    setInput2("");
   };
+
+  // function reset() {
+  //   setInput("");
+  //   setInput2("");
+  // }
 
   return (
     <div>
@@ -39,10 +47,16 @@ function App() {
         onChange={(e) => setInput(e.target.value)}
         placeholder="Enter a number"
       />
-      <button onClick={() => handleOperation('+')}>+</button>
-      <button onClick={() => handleOperation('-')}>-</button>
-      <button onClick={() => handleOperation('*')}>*</button>
-      <button onClick={() => handleOperation('/')}>/</button>
+      <input
+        type="text"
+        value={input2}
+        onChange={(e) => setInput2(e.target.value)}
+        placeholder="Enter a number"
+      />
+      <button onClick={() => handleOperation("+")}>+</button>
+      <button onClick={() => handleOperation("-")}>-</button>
+      <button onClick={() => handleOperation("*")}>*</button>
+      <button onClick={() => handleOperation("/")}>/</button>
       <button onClick={() => dispatch(reset())}>C</button>
       <p>Result: {result}</p>
     </div>
--- calculator/src/Features/Calculator/CalculatorSlice.jsx
+++ calculator/src/Features/Calculator/CalculatorSlice.jsx
@@ -4,22 +4,23 @@ const calculatorSlice = createSlice({
   name: 'calculator',
   initialState: {
     result: 0,
+   
   },
   reducers: {
     toplama: (state, action) => {
-      state.result += action.payload;
+      state.result = action.payload.input + action.payload.input2;
     },
     cixma: (state, action) => {
-      state.result -= action.payload;
+      state.result = action.payload.input - action.payload.input2;
     },
     vurma: (state, action) => {
-      state.result *= action.payload;
+      state.result = action.payload.input * action.payload.input2;
     },
     bolme: (state, action) => {
-      state.result /= action.payload;
+      state.result = action.payload.input / action.payload.input2;
     },
     reset: (state) => {
-      state.result = 0;
+      state.result =0;
     },
   },
 });
